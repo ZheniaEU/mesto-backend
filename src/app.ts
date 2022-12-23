8f2017c626b7842fe33604629eea02853a786daa
Zhenia <zhenia1985@gmail.com>
2022-12-23 16:18:38 +0700

    fix for me, fix eslint

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,37 @@
 import express, { Request, Response, Router, json } from "express"
+
 import path from "path"
+
 import { users } from "./db"
-import mongoose from "mongoose"
 
 const { PORT = 3000 } = process.env
 
 const app = express()
-
 
 app.use(json())
 
 const router = Router()
 
 router.get("/users", (req: Request, res: Response) => {
-
    res.status(200).send(users)
 })
 
 router.post("/users/:id", (req: Request, res: Response) => {
 
    try {
-
       const { id } = req.params
 
       const newUser = { ...req.body, id }
 
       users.push(newUser)
       res.status(201).send({ status: "проверка добавления" })
-
    } catch (e) {
       res.status(500).send({ message: "у нас проблемы с добовлением" })
-
    }
-
 })
-
 
 app.use(router)
 
-
 app.use(express.static(path.join(__dirname, "public")))
-app.listen(PORT, () => {
-   console.log("сервер слушает на по порту:", PORT)
-})
+
+app.listen(PORT, () => { })
