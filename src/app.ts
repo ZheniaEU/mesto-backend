1d9202e3635ba58f24020351d0e5e49b6fcaae30
Zhenia <zhenia1985@gmail.com>
2022-12-24 09:56:48 +0700

    fix eslint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 import express, { Request, Response, Router, json } from "express"
-
 import path from "path"
-
-import { users } from "./db"
+import { users as db } from "./db"
 
 const { PORT = 3000 } = process.env
 
@@ -13,19 +11,18 @@ app.use(json())
 const router = Router()
 
 router.get("/users", (req: Request, res: Response) => {
-   res.status(200).send(users)
+   res.status(200).send(db)
 })
 
 router.post("/users/:id", (req: Request, res: Response) => {
 
    try {
       const { id } = req.params
-
       const newUser = { ...req.body, id }
 
-      users.push(newUser)
+      db.push(newUser)
       res.status(201).send({ status: "проверка добавления" })
-   } catch (e) {
+   } catch {
       res.status(500).send({ message: "у нас проблемы с добовлением" })
    }
 })
