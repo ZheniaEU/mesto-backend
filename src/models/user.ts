919c6f5587f50f43024bf0b6ca94d90a383834a2
Zhenia <zhenia1985@gmail.com>
2023-01-04 05:35:53 +0700

    feat: add user email, password with regex

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,12 +1,29 @@
 import { model, Schema } from "mongoose"
 
 export type User = {
+   email: string
+   password: string
    name: string
    about: string
    avatar: string
 }
 
 const userSchema = new Schema<User>({
+   email: {
+      type: String,
+      unique: true,
+      validate: {
+         validator: (str: string) => /^[^.](?=[a-z\d!#$%&'*+\-\\/=?.^_`{}|~]+@([a-z-.\d]+\.)+[a-z]{2,}$)((?!\.\.).)*$/i.test(str)
+      },
+      message: "Ваша почта не подходит"
+   },
+   password: {
+      type: String,
+      validate: {
+         validator: (str: string) => /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[#?!@$%^&*-])[a-zA-Z\d#?!@$%^&*-]{8,16}$/.test(str)
+      },
+      message: "Пароль должен быть от 8 до 16 символов. А так же как минимум содержать одну заглавную букву, цифру, а так де спец символ"
+   },
    name: {
       type: String,
       require: true,
