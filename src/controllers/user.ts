8a3131788228d357eec7396aa3ddc561b9bbc073
Zhenia <zhenia1985@gmail.com>
2023-01-06 20:44:33 +0700

    feat: improved controllers user

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,6 +1,13 @@
 import { modelUser as User } from "../models/user"
+import dotenv from "dotenv"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 import type { NextFunction, Request, Response } from "express"
+
+dotenv.config()
+
+const { JWT_SECRET = "secret" } = process.env
 
 export const getUsers = (_: Request, res: Response, next: NextFunction) => {
 
@@ -16,11 +23,19 @@ export const getUsersByID = (req: Request, res: Response, next: NextFunction) =>
       .catch((err) => next(err))
 }
 
-export const createUser = (req: Request, res: Response, next: NextFunction) => {
+//! дописать
+// export const createUser = (req: Request, res: Response, next: NextFunction) => {
+export const createUser = async (req: Request, res: Response) => {
 
-   User.create(req.body)
-      .then((newUser) => res.status(200).send({ data: newUser }))
-      .catch((err) => next(err))
+   const { email, password, name, avatar, about } = req.body
+
+   return bcrypt.hash(password, 10)
+      .then((hash: string) => User.create({ email, password: hash, name, avatar, about }))
+      .then((user) => { res.status(201).send({ data: user }) })
+      .catch((err) => { res.status(400).send(err) })
+   // User.create(req.body)
+   //    .then((newUser) => res.status(200).send({ data: newUser }))
+   //    .catch((err) => next(err))
 }
 
 export const updateUser = (req: Request, res: Response, next: NextFunction) => {
@@ -37,4 +52,19 @@ export const updateAvatar = (req: Request, res: Response, next: NextFunction) =>
    User.findByIdAndUpdate(req.user._id, req.body.avatar, { new: true, runValidators: true })
       .then((user) => res.status(200).send({ data: user }))
       .catch((err) => next(err))
+}
+
+// ? чё с мидлваром? он нам нужен?
+export const login = (req: Request, res: Response) => {
+
+   const { email, password } = req.body
+
+   User.findUserByCredentials(email, password)
+      .then((user) => {
+
+         const token = jwt.sign({ _id: user._id }, JWT_SECRET, { expiresIn: "7d" })
+
+         res.status(200).send({ token })
+      })
+      .catch((err) => { res.status(401).send({ message: err.message }) })
 }
